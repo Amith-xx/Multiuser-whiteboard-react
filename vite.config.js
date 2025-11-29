18f91bd6c8ead634e32d05623196e2d3d790e345
Amith-xx <aa2991333@gmail.com>
2025-11-30 01:43:41 +0530

    Vite modified

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-
   plugins: [react()],
-
-  server:{
-    proxy:{
-      '/ws':{
-        target: 'ws://localhost:8080',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  },
-  define:{
-    global:'window',
-  }
+  base: '/Multiuser-whiteboard-react/',  //  <<< IMPORTANT !!!
 })
